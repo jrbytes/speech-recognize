b629e86f8f36642f68b0bfc3c970fc503b05e883
Jr Bytes <itamazor@gmail.com>
2020-11-09 19:23:40 -0300

    Added noalsaerr file to handle with errors alsa in linux. Updated main code to open programs.

--- speech_recognize.py
+++ speech_recognize.py
@@ -1,8 +1,10 @@
 import speech_recognition as sr
+import os
+from err import noalsaerr
 
 def main():
   mic = sr.Recognizer()
-  with sr.Microphone() as source:
+  with noalsaerr.noalsaerr() as n, sr.Microphone() as source:
     mic.adjust_for_ambient_noise(source)
     print(f'Fale algo: ')
     audio = mic.listen(source)
@@ -10,6 +12,11 @@ def main():
   try:
     text = mic.recognize_google(audio, language = "pt-BR")
     print(f'Você disse: {text}')
+
+    lowercase_letters = text.lower()
+
+    if lowercase_letters == 'abrir spotify':
+      print(os.system('spotify'))
   except sr.UnkownValueError:
     print(f'Não entendi')
 
